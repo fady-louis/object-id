5d285d72b37ef911958be9e74e09f262885606cc
fady-louis <99764756+fady-louis@users.noreply.github.com>
2022-05-25 22:53:19 +0200

    Add files via upload

---
+++ DisplayMessage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Reflection;
+
+namespace displayprob
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class DisplayMessage : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                //get UIdoc
+                UIDocument uidoc = commandData.Application.ActiveUIDocument;
+                //pickobj
+                Reference pickObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                //show obj
+                if (pickObj != null)
+                {
+                    TaskDialog.Show("Element Id", pickObj.ElementId.ToString());
+                }
+                return Result.Succeeded;
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ ExternalApplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Reflection;
+
+namespace displayprob
+{
+    class ExternalApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //Create Ribbon Tab
+            application.CreateRibbonTab("My Commands");
+
+            string path = Assembly.GetExecutingAssembly().Location;
+            PushButtonData button = new PushButtonData("show ID", "Your Element", path, "displayprob.DisplayMessage");
+
+            RibbonPanel panel = application.CreateRibbonPanel("enteer your element", "element");
+
+            panel.AddItem(button);
+
+            return Result.Succeeded;
+        }
+    }
+}
